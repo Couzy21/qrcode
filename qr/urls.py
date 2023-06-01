fe7dfd74c4ab8509cd7f02804ae70cc27f1108c5
couzy <courageabudu@gmail.com>
2023-06-02 00:05:14 +0100

    Fixed authentication

--- qr/urls.py
+++ qr/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home, login, signup
+from .views import home, login_view, signup
 app_name = 'qr'
 urlpatterns = [
     path("home/", home, name="home"),
-    path('login/', login, name="login"),
+    path('login/', login_view, name="login"),
     path('signup/', signup, name="signup"),
 ]
